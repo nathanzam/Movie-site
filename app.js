0a21827ba402c4952e6ae09f9f6f9a1d7e3ace63
nathanzam <32460158+nathanzam@users.noreply.github.com>
2017-11-29 01:09:26 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();
-});
+(function() {
+  'use strict';
+
+  var app = angular.module('shawshank', ['store-products'])
+
+    app.controller("StoreCtrl", function($http, $scope) {
+      $http.get("./reviews.json").then(function (response) {
+        $scope.products = response.data;
+
+      });
+    })
+
+    app.controller("ReviewCtrl");
+
+}());
