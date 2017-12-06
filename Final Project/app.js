d1ce842acc3431d4cdb4dc82b7a0027c097a70c4
nathanzam <32460158+nathanzam@users.noreply.github.com>
2017-12-06 14:53:45 -0500

    Add files via upload

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,26 +1,33 @@
 (function() {
   'use strict';
 
-  var app = angular.module('shawshank', ['store-products'])
+  var app = angular.module('shawshank', ['store-products']);
 
-    app.controller("StoreCtrl", function($http, $scope) {
-      $http.get("./reviews.json").then(function (response) {
-        $scope.products = response.data;
 
-      });
-    })
-
-    app.controller('MovieCtrl', function($scope, $http) {
-
-      $scope.today = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
-
-      $scope.queryZip = function(zip) {
-        $http.get('http://data.tmsapi.com/v1.1/movies/showings?api_key=27ax2xgbtpbdkbv8337nf4p9&zip=' + $scope.userInput + '&startDate=' + today).then(function(response) {
-          $scope.movies = response.data;
-        });
-      };
+  app.controller("StoreCtrl", function($http, $scope) {
+    $http.get("./reviews.json").then(function(response) {
+      $scope.products = response.data;
     });
+  });
 
-    app.controller("ReviewCtrl");
+  app.controller('MovieCtrl', function($scope, $http) {
+
+    var d = new Date();
+    var today = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+    $scope.today = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+    $scope.queryZip = function(zip) {
+      $http.get('http://data.tmsapi.com/v1.1/movies/showings?api_key=gyqhhfjsvbzvhqgm8rjjpvrk&zip=' + $scope.userInput + '&startDate=' + today).then(function(response) {
+        $scope.movies = response.data;
+      });
+    };
+  });
+
+  app.controller("ReviewCtrl");
 
 }());
+
+var apikey = "gyqhhfjsvbzvhqgm8rjjpvrk";
+// var apikey = "gkj8dsj2ha89jwfu3x59p2sh";
+// var apikey = "vzxkgktf4t6jmkqnkv39c3zh";
